d785d43c579ae13b86683e5eaa2c08dcf2e683dd
avsh <avsh@users.noreply.github.com>
2026-01-07 13:09:32 +0300

    add composer

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'begateway/virtuemart-payment-module',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f3141b175bb028cbd168e0f2675dfe8d80317e91',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'begateway/begateway-api-php' => array(
+            'pretty_version' => '5.1.1',
+            'version' => '5.1.1.0',
+            'reference' => '0238d5d45ee78cd02470fc22b75def380da345e3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../begateway/begateway-api-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'begateway/virtuemart-payment-module' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f3141b175bb028cbd168e0f2675dfe8d80317e91',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
